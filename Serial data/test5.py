aefae05ed4c839548300c48a4273ed056f7fc5e6
a3539450 <a3539450@gmail.com>
2018-12-13 07:56:42 +0800

    Fuck Error

--- Serial data/test5.py
+++ Serial data/test5.py
@@ -8,19 +8,18 @@ import socket
 import serial
 #import datetime
 
-#sys.path.insert(0, '/usr/lib/python2.7/bridge/')  # Type Bridge
-#from bridgeclient import BridgeClient as bridgeclient
-
 global connect 
 host='http://www.google.com/'
-deviceId = "DZv4BltK"
-deviceKey = "rXBX27o4aGY7SnPa"
+deviceId = "DpjBrN1w"
+deviceKey = "0FQSO35FKLFkjpLF"
 ip = "api.mediatek.com"
 port = "80"
 timeout=5
 connect = 1
 Serial2 = 115200
-srl = serial.Serial('/dev/ttyS0',Serial2)
+# port = '/dev/ttyS0' ## Unix 7688
+port = 'COM5' ## Windows System
+srl = serial.Serial(port,Serial2)
 def connected():
     global connect
     while True:
@@ -37,19 +36,21 @@ Thread1 = threading.Thread(target = connected)
 
 def csvfile(table,payload):   
     if connect == 1:
-        #with open('/MCS/output1.csv', 'a') as csvfile: ##Python 2.7 Unix
+        # with open('/MCS/output1.csv', 'a') as csvfile: ##Python 2.7 Unix
         with open('C:\python\data.csv ', 'a', newline='' ) as csvfile: ##Python 3.x Windows
             writer2 = csv.writer(csvfile)
             writer2.writerows(table)
         csvfile.closed
     else :
-        with open('C:\Python\casd.csv', newline='') as readcsvfile:
-            read1 = csv.reader(readcsvfile)
-            for i,row in enumerate(read1)
-                if i <= 3:
-                row = rows
-                data = row[0] + ',' + row[1] + ',' + row[2]
-                print(data)
+        # with open('/MCS/output1.csv') as readcsvfile:
+        # with open('C:\Python\casd.csv', newline='') as readcsvfile:
+        #     read1 = csv.reader(readcsvfile)
+        #     for i,row in enumerate(read1)
+        #         if i <= 3:
+        #         row = rows
+        #         data = row[0] + ',' + row[1] + ',' + row[2]
+        #         print(data)
+        #     readcsvfile.close
         headers = {"Content-type": "text/json", "deviceKey": deviceKey}
         conn = http.client.HTTPConnection(ip + ":" + port, timeout=30)
         try:
@@ -58,62 +59,68 @@ def csvfile(table,payload):
             response = conn.getresponse()
             print( response.status, response.reason, payload, time.strftime("%c"))
             conn.close  
-            #http.client.HTTPConnection.close()
         except (http.client.HTTPException, socket.error) as ex:
-            #with open('/MCS/output1.csv', 'a') as csvfile: ##Python 2.7 Unix
+            # with open('/MCS/output1.csv', 'a') as csvfile: ##Python 2.7 Unix
             with open('C:\python\data.csv', 'a', newline='')as csvfile1: ##Python 3.x Windows
                 writer1 = csv.writer(csvfile1)
                 writer1.writerows(table)
             csvfile.close
-            #with open('/log/error.log', 'a') as log: ## Python 2.7 Unix
+            # with open('/log/error.log', 'a') as log: ## Python 2.7 Unix
             with open('C:\python\error.log', 'a', newline='') as log: ## Python 3.x Windows
                 error = log.writer(log)
                 error.writerow("Error: %s" % ex)
-
-def data():
-    if connect == 1:
-        
-
-
-
 def ttes():
     while True:
-        int D1, D2, D3, D4, D5, D6, D7, D8, D9, E1, E2, E3
+        p1 = str(int(time.time()))
+        int DA = 0
+        int DB = 0
+        int DC = 0
+        int DD = 0
+        int DE = 0
+        int DF = 0
         data = str(ser.readline())
         dataa = data.replace("\n","")
         data1 = dataa.split(",")
         for i,rows in enumerate(data1):
             if i == 0:
-                D1 = rows[0:5]
+                DA = rows[0:5]
             else:
-                D1 = '0'
+                DA = '0'
             if i == 1:
-                D2 = rows[0:5]
+                DB = rows[0:5]
             else:
-                D2 = '0'
+                DB = '0'
             if i == 2:
-                D3 = rows[0:5]
+                DC = rows[0:5]
             else:
-                D3 = '0'
+                DC = '0'
             if i == 3:
-                D4 = rows[0:5]
+                DD = rows[0:5]
             else:
-                D4 = '0'
+                DD = '0'
             if i == 4:
-                D5 = rows[0:5]
+                DE = rows[0:5]
             else:
-                D5 = '0'
+                DE = '0'
             if i == 5:
-                D6 = rows[0:5]
+                DF = rows[0:5]
             else:
-                D6 = '0'
-        D1String = "D1,," + D1
-        D2String = "D2,," + D2
-        D3String = "D3,," + D3
-        D4String = "D4,," + D4
-        D5String = "D5,," + D5
-        D6String = "D6,," + D6
+                DF = '0'
+        D1String = "D1,," + DA
+        D2String = "D2,," + DB
+        D3String = "D3,," + DC
+        D4String = "D4,," + DD
+        D5String = "D5,," + DE
+        D6String = "D6,," + DF
         payload = D1String + "\n" + D2String + "\n" + D3String + "\n" + D4String + "\n" + D5String + "\n" + D6String + "\n"
+        table = [
+        ["D1",p1,DA],
+        ["D2",p1,DB],
+        ["D3",p1,DC],
+        ["D4",p1,DD],
+        ["D5",p1,DE],
+        ["D6",p1,DF],
+        ]
         # p1 = str(int(time.time()))
         # t0String = "temp,," + t0
         # h0String = "humi,," + h0
